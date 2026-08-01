430c5b4f5e721f6743aeae6169f375f4f4967a57
rolando2012 <rolamdomr@gmail.com>
2026-08-01 09:36:22 -0400

    setup model, resource routes and form requests validation

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
+Route::resource('categories',CategoryController::class);
