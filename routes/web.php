cdd86783866bed1809f5efa8a326e94aba13579f
rolando2012 <rolamdomr@gmail.com>
2026-08-05 14:32:50 -0400

    feat: add Tag model with migration and controller

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
 Route::resource('categories',CategoryController::class);
+Route::resource('tags', TagController::class);
