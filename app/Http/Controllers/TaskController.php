bf62c927ee889af6aeee9e5fd39edda6d2db2d6e
rolando2012 <rolamdomr@gmail.com>
2026-08-08 09:17:44 -0400

    feat: implement task deletion functionality

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -62,6 +62,7 @@ class TaskController extends Controller
 
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+        return redirect()->route('tasks.index')->with('success', 'Tarea eliminada correctamente.');
     }
 }
