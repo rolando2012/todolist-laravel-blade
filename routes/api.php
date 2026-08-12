586ef13fe3d6f10573d186b7af1b9b0d716a1cd6
rolando2012 <rolamdomr@gmail.com>
2026-08-11 23:23:07 -0400

    feat: Add TagController and API routes for tags

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\TagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('categories',CategoryController::class);
+Route::apiResource('tags',TagController::class);
