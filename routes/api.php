d5bfe60540b8047af900a234e4ff6a7e0b29280e
rolando2012 <rolamdomr@gmail.com>
2026-08-17 14:54:40 -0400

    feat: group api resources under sanctum middleware

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,11 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::apiResource('categories',CategoryController::class);
-Route::apiResource('tags',TagController::class);
-Route::apiResource('tasks',TaskController::class);
+Route::middleware('auth:sanctum')->group(function(){
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('categories',CategoryController::class);
+    Route::apiResource('tags',TagController::class);
+    Route::apiResource('tasks',TaskController::class);
+});
